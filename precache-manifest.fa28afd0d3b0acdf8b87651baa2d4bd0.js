62fa36c260c942309519fd0047ef672af0beb035
Timyr4ikon <54175850+Timyr4ikon@users.noreply.github.com>
2020-06-21 15:29:30 +0300

    Updates

---
+++ precache-manifest.fa28afd0d3b0acdf8b87651baa2d4bd0.js
@@ -0,0 +1,46 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "b291c550536584fd519f45568f36b8e9",
+    "url": "/goit-react-hw-02-ph0nebook/index.html"
+  },
+  {
+    "revision": "cb5b100eace7a7a8f8f3",
+    "url": "/goit-react-hw-02-ph0nebook/static/css/2.22fb77fa.chunk.css"
+  },
+  {
+    "revision": "e1729a5dea350cd8ac2c",
+    "url": "/goit-react-hw-02-ph0nebook/static/css/main.4f8242b2.chunk.css"
+  },
+  {
+    "revision": "cb5b100eace7a7a8f8f3",
+    "url": "/goit-react-hw-02-ph0nebook/static/js/2.afb3cbdd.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/goit-react-hw-02-ph0nebook/static/js/2.afb3cbdd.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "e1729a5dea350cd8ac2c",
+    "url": "/goit-react-hw-02-ph0nebook/static/js/main.5d02be43.chunk.js"
+  },
+  {
+    "revision": "705f1f6ceb4b58f3ae36",
+    "url": "/goit-react-hw-02-ph0nebook/static/js/runtime-main.3999d31f.js"
+  },
+  {
+    "revision": "0509ab09c1b0d2200a4135803c91d6ce",
+    "url": "/goit-react-hw-02-ph0nebook/static/media/MaterialIcons-Regular.0509ab09.woff2"
+  },
+  {
+    "revision": "29b882f018fa6fe75fd338aaae6235b8",
+    "url": "/goit-react-hw-02-ph0nebook/static/media/MaterialIcons-Regular.29b882f0.woff"
+  },
+  {
+    "revision": "96c476804d7a788cc1c05351b287ee41",
+    "url": "/goit-react-hw-02-ph0nebook/static/media/MaterialIcons-Regular.96c47680.eot"
+  },
+  {
+    "revision": "d120c85b6eb0549d60483e2980033c15",
+    "url": "/goit-react-hw-02-ph0nebook/static/media/MaterialIcons-Regular.d120c85b.ttf"
+  }
+]);
